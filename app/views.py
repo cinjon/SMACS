5a19dfcdcf2c124b0db89b3c87a11aa62b55d32b
cinjon <cinjon.resnick@gmail.com>
2014-01-02 09:15:00 -0800

    shows base, etc

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 import app
 from flask import render_template, url_for, redirect, g
-from flask.ext.login import logout_user, login_required, login_user
+# from flask.ext.login import logout_user, login_required, login_user
+
+@app.flask_app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
