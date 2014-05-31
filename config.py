8c939b4314c91865cbdccb6ec7734a5f07c32d8b
cinjon <cinjon.resnick@gmail.com>
2014-05-31 07:04:50 -0700

    config and lib files

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ SECRET_KEY = os.environ.get('SMACS_SECRET_KEY')
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = os.environ.get('SMACS_DATABASE_URL', None)
+
+# API Requests: gets filled in by /model and implemented by /controller
+API_MODELS = {}
+CRUD_URL_MODELS = {}
