015101a0b9d759c6b34381ec7e8a59c8901641f3
cinjon <cinjon.resnick@gmail.com>
2014-06-03 15:28:05 -0700

    angular services/controllers/directies: getting the hang of it now. need to refactor a bunch around home because there's duplicate code

--- app/static/js/modules.js
+++ app/static/js/modules.js
@@ -1,38 +1,86 @@
 'use strict';
 
-angular.module('SmacDB', ['smacServices', 'ngResource', 'ngRoute', 'typeahead'])
+angular.module('SmacDB', ['ui.bootstrap', 'smacServices', 'ngResource', 'ngRoute'])
+  .controller('landing', function($scope, $resource) {
+    $scope.loginUser = function() {
+      console.log('logging in user ' + $scope.email + ' : ' + $scope.password);
+    }
+  })
+  .controller('about', function($scope) {
+  })
+  .controller('home', function($scope, $http, $location, limitToFilter) {
+    $scope.showDrugList = true;
+    $scope.handleSelection = function(item) {
+      return $http.get('/drug_unique_id/' + item.type + '/' + item.name, {}).then(function(result) {
+        console.log(result);
+        $location.url('/drug/' + result.data.unique_id);
+      });
+    };
+    $scope.getGenerics = function(val) {
+      return $http.get('/api/typeahead-generics?q={"filters":[{"name":"label_name", "op":"is_null"}], "query":"' + val + '"}', {}).then(function(result) {
+        return limitToFilter(result.data.objects, 10);
+      });
+    };
+    $scope.getLabels = function(val) {
+      return $http.get('/api/typeahead-labels?q={"filters":[{"name":"label_name", "op":"is_not_null"}], "query":"' + val + '"}', {}).then(function(result) {
+        return limitToFilter(result.data.objects, 10);
+      });
+    };
+    $http.get('/api/random-drugs?q={"number":10, "type":"generic_name"}', {}).then(function(result) {
+      $scope.randomGenerics = result.data.objects;
+    });
+    $http.get('/api/random-drugs?q={"number":10, "type":"label_name"}', {}).then(function(result) {
+      console.log(result);
+      $scope.randomLabels = result.data.objects;
+    });
+  })
+  .controller('drugDetail', function($scope, $routeParams, Drug) {
+    var drugQuery = Drug.get({drug_id: $routeParams.drug_id}, function(drug) {
+      console.log(drug);
+      $scope.drug = drug;
+    });
+  })
+  .controller('drugList', function($scope, Drug) {
+    var drugsQuery = Drug.get({}, function(drugs) {
+      $scope.drugs = drugs.objects;
+    });
+  })
   .config([
     '$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
       $routeProvider
 	.when('/', {
 	  templateUrl: 'static/partials/landing.html',
-	  controller: IndexController
+          controller: 'landing'
 	})
 	.when('/about', {
 	  templateUrl: 'static/partials/about.html',
-	  controller: AboutController
+          controller: 'about'
 	})
         .when('/drug', {
-          templateUrl: 'static/partials/drug-list.html',
-          controller: DrugListController
+          templateUrl: '/static/partials/drug-list.html',
+          controller: 'drugList'
         })
-        .when('/drug/:drugId', {
-          templateUrl: 'static/partials/drug-detail.html',
-          controller: DrugDetailController
+        .when('/drug/:drug_id', {
+          templateUrl: '/static/partials/drug-detail.html',
+          controller: 'drugDetail'
         })
         .when('/home', {
           templateUrl: 'static/partials/home.html',
-          controller: HomeController
+          controller: 'home'
         })
 	.otherwise({
 	  redirectTo: '/'
 	});
       $locationProvider.html5Mode(true);
     }
-  ])
-  .run(function($rootScope, $templateCache) {
-    $rootScope.$on('$viewContentLoaded', function() {
-      $templateCache.removeAll();
-    });
-  });
+  ]);
+
+// angular.module("template/home/drug-result.html", []).run(["$templateCache", function($templateCache) {
+//   $templateCache.put("template/home/drug-result.html",
+//     "<ul class=\"dropdown-menu\" ng-style=\"{display: isOpen()&&'block' || 'none', width:100%, top: position.top+25+'px'}\">\n" +
+//     "    <li ng-repeat=\"match in matches\" ng-class=\"{active: isActive($index) }\" ng-mouseenter=\"selectActive($index)\" ng-click=\"selectMatch($index)\">\n" +
+//     "        <div typeahead-match index=\"$index\" match=\"match\" query=\"query\" template-url=\"templateUrl\"></div>\n" +
+//     "    </li>\n" +
+//     "</ul>");
+// }]);
--- app/static/js/services.js
+++ app/static/js/services.js
@@ -2,29 +2,13 @@
 
 angular.module('smacServices', ['ngResource'])
   .factory('Drug', function($resource) {
-    return $resource('/api/drug/:drugId', {}, {
+    return $resource('/api/drug/:drug_id', {}, {
       query: {
 	method: 'GET',
-	params: {drugId:''},
+	params: {drug_id:''},
 	isArray: true
       }
     });
-  })
-  .factory('typeaheadGenerics', function($resource) { //TODO: limit this to not be regular accessible
-    return $resource('/api/typeahead-generics?q={"filters":[{"name":"label_name", "op":"is_null"}]}', {}, {
-      query: {
-	method: 'GET',
-	isArray: true
-      }
-    })
-  })
-  .factory('typeaheadLabels', function($resource) { //TODO: limit this to not be regular accessible
-    return $resource('/api/typeahead-labels?q={"filters":[{"name":"label_name", "op":"is_not_null"}]}', {}, {
-      query: {
-	method: 'GET',
-	isArray: true
-      }
-    })
   })
   .factory('subscriptionQuery', function($resource) {
     // Returns the current user's subscriptions
